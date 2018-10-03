9296ae7a305a32881fd72a93a7829be0e54addca
Reached <casper.aarby.sorensen@gmail.com>
2018-10-03 16:39:14 +0200

    Initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <Page>
+        <StackLayout>
+            <SegmentedBar>
+                <SegmentedBarItem title="First" />
+                <SegmentedBarItem title="Second" />
+                <SegmentedBarItem title="Third" />
+            </SegmentedBar>
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
